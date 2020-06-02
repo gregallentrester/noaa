1299954e9f051e51cefd87400f56430a6918529c
greg <greg@gregs-MacBook-Pro.local>
2020-06-02 05:54:48 -0500

    added missing validation for 5-day span

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  NOAA Forecast - Broadcom
 ---
 ##### Dry Run
-After cloning the code into this directory: 
+After cloning the code into this directory:
 
     ~/stage/broadcom/
 
@@ -11,7 +11,7 @@ A dry run can be performed by sourcing this script:
     cd ~/stage/broadcom/
 
     . ok
-  
+
 ---
 ##### ßuild
 A ßuild can be performed in the directory \
@@ -19,12 +19,18 @@ A ßuild can be performed in the directory \
     cd ~/stage/broadcom/
 
     . bld
-  
+
   ---
 ##### Execute
-Execute the code with formal, CLI arguments (syntax is ordained by Spring 2.x) 
+Execute the code with formal, CLI arguments (syntax is ordained by Spring 2.x)
 by supplying a set of comma-separated, quoted geocodes in this order  N-Coor, W-Coor:
 
     cd ~/stage/broadcom/
 
     mvn spring-boot:run -q -B -Dspring-boot.run.arguments="37.3541,121.9552"
+
+    mvn spring-boot:run -q -B -Dspring-boot.run.arguments="61.17,150"
+    mvn spring-boot:run -q -B -Dspring-boot.run.arguments="40.7306,-73.9352"
+
+    mvn spring-boot:run -q -B -Dspring-boot.run.arguments="39.7456,-97.0892"
+    mvn spring-boot:run -q -B -Dspring-boot.run.arguments="37.3541,-121.9552"
--- src/main/java/net/greg/examples/salient/NOAAService.java
+++ src/main/java/net/greg/examples/salient/NOAAService.java
@@ -1,5 +1,6 @@
 package net.greg.examples.salient;
 
+// more than 7 imports from same package s.b. *
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -8,48 +9,112 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import java.time.*;
+import java.time.format.*;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public final class NOAAService {
 
-
+  /**
+   * Produce 7-day forecast
+   * @param args CLI formal argument (in accordance with Spring2)
+   *             is a single, comma-separated value
+   */
   public static void main(String[] args) {
     new NOAAService().accept(args);
-
   }
 
+  /**
+   * Coordinates (nested) calls to <code>forecast()</code>,
+   * <code>locate()</code> member methods, rites results to a flat file
+   *
+   * @param args passed CLI formal argument (in accordance with Spring2)
+   *             is a single, comma-separated value
+   * @see #main(String[])
+   */
   private void accept(String[] args) {
 
-//locate(args);
-//*
-    String forecasts = forecast(locate(args));
+    COORS_SENTINEL = args[0];
 
-    System.err.println(forecasts);
+    String forecast = forecast(locate(args));
 
     try (
       BufferedWriter publisher =
           new BufferedWriter(
-            new FileWriter(REPRESENTATION))) {
+            new FileWriter(HTML_REPRESENTATION))) { // NOAAForecast.HTML
 
-      publisher.write(forecasts);
+      publisher.write(forecast);
     }
     catch(IOException e) { e.printStackTrace(); }
-
-  //  */
   }
 
-
+  /**
+   * Find the stringified JSON payload based upon the deciphered locale
+   *
+   * @param location the deciphered locale
+   * @return the stringified JSON payload
+   * @see #accept(String[])
+   * @see #locate(String[])
+   */
   public static String forecast(String location) {
+
+    String TODAY = LocalDate.now().getDayOfWeek().name();
+
+    System.err.println(
+      report.append(
+      "\n Today ").append(GRN).
+      append(LocalDate.now().getDayOfWeek().name()).
+      append(NC).
+      append(localeDesignation).toString());
+
+    if (TODAY.equalsIgnoreCase("MONDAY")) {
+      LOWER_BOUNDS = 1;
+      UPPER_BOUNDS = 10;
+    }
+    else if (TODAY.equalsIgnoreCase("TUESDAY")) {
+      LOWER_BOUNDS = 2;
+      UPPER_BOUNDS = 11;
+    }
+    else if (TODAY.equalsIgnoreCase("WEDNESDAY")) {
+      LOWER_BOUNDS = 3;
+      UPPER_BOUNDS = 12;
+    }
+    else if (TODAY.equalsIgnoreCase("THURSDAY")) {
+      LOWER_BOUNDS = 4;
+      UPPER_BOUNDS = 13;
+    }
+    else if (TODAY.equalsIgnoreCase("FRIDAY")) {
+      LOWER_BOUNDS = 5;
+      UPPER_BOUNDS = 14;
+    }
+    else if (TODAY.equalsIgnoreCase("SATURDAY")) {
+      LOWER_BOUNDS = 6;
+      UPPER_BOUNDS = 15;
+    }
+    else if (TODAY.equalsIgnoreCase("SUNDAY")) {
+      LOWER_BOUNDS = 7;
+      UPPER_BOUNDS = 16;
+    }
 
     boolean accum = false;
     String line = "";
+
     StringBuilder forecasts =
       new StringBuilder(PREAMBLE);
+
+
+    if (null == location) {
+
+      System.err.println(
+        RED + STOP + " " + COORS_SENTINEL + NC);
+
+      return
+        PREAMBLE + "<li><span style='color:red;'>" +
+        STOP + "&nbsp;" + COORS_SENTINEL + "</span>";
+    }
 
     try {
 
@@ -61,15 +126,30 @@ public final class NOAAService {
           new InputStreamReader(
             process.getInputStream()));
 
+      int ndx = 0;
+
       while (null != (line = br.readLine())) {
 
         if (line.contains(PERIODS_TOKEN)) {
           accum = true;
         }
 
+        if (line.contains(NUMBER_LBL)) {
+
+          ndx =
+            Integer.parseInt(
+              line.substring(
+                line.indexOf(":")+1,line.length()-1).trim());
+
+          if (ndx > UPPER_BOUNDS) {
+            return forecasts.toString();
+          }
+        }
+
         if (accum &&
             ! line.contains(NUMBER_LBL) &&
-            ! line.contains(PERIODS_TOKEN)) {
+            ! line.contains(PERIODS_TOKEN) &&
+            ndx >= LOWER_BOUNDS) {
 
           line = line.replace(']', ' ');
           line = line.replace('[', ' ');
@@ -88,6 +168,7 @@ public final class NOAAService {
             forecasts.append(HR + "\n<li><b><br/>" + line.trim() + "</b>");
           }
           else if (line.contains(ICON_LBL)) {
+
             line = line.substring(line.indexOf(":")+1);
 
             forecasts.append("\n<li><img src='" + line.trim());
@@ -104,19 +185,18 @@ public final class NOAAService {
     return forecasts.toString();
   }
 
-
   /**
    * Detect NOAA locale via this endpoint.
    *
    * @param args coordinates via CLI
    * @return the NOAA forecast endpoint
+   * @see #accept(String[])
    */
   private String locate(String[] args) {
 
-    String answer = "";
+    String locale = STOP;
 
     ENDPOINT_POINTS.append(args[0]);
-
 
     try {
 
@@ -128,28 +208,42 @@ public final class NOAAService {
           new InputStreamReader(
             process.getInputStream()));
 
-      while (null != (answer = br.readLine())) {
+      while (null != (locale = br.readLine())) {
 
-        if (answer.contains(FORECAST_KEY)) {
+        if (locale.contains(FORECAST_KEY)) {
 
-          answer =
-            answer.substring(
-              answer.indexOf(HTTPS_TOKEN),
-              answer.lastIndexOf("\""));
+          locale =
+            locale.substring(
+              locale.indexOf(HTTPS_TOKEN),
+              locale.lastIndexOf("\""));
 
-          System.err.println(GRN + answer + NC);
+          localeDesignation =
+            "\nLocale " + GRN + locale + NC;
 
-          return answer;
+          return locale;
         }
       }
     }
     catch (IOException e) { e.printStackTrace(); }
 
-    return answer;
+    return locale;
   }
 
 
-  private static final String REPRESENTATION =
+  private static String localeDesignation;
+
+  private static  StringBuilder report =
+    new StringBuilder(100);
+
+  private static String COORS_SENTINEL = "";
+
+  private static int LOWER_BOUNDS;
+  private static int UPPER_BOUNDS;
+
+  private static final String STOP =
+    "Invalid Coordinates";
+
+  private static final String HTML_REPRESENTATION =
     "NOAAForecast.HTML";
 
   private static String CURL = "curl";
@@ -173,12 +267,13 @@ public final class NOAAService {
     "' width='30' height='30' style='border-radius: 50%;'/>";
 
   private static final String HR =
-    "<hr style='border:0; height:1px;width:95%;margin-top:1.4em;"+
+    "<hr style='border:0; height:1px;width:95%;margin-top:1.8em;"+
     "background-image:-webkit-linear-gradient(left,#f0f0f0,#8c8b8b,#f0f0f0);"+
     "background-image:-moz-linear-gradient(left,#f0f0f0,#8c8b8b,#f0f0f0);"+
     "background-image:-ms-linear-gradient(left,#f0f0f0,#8c8b8b,#f0f0f0);"+
     "background-image:-o-linear-gradient(left,#f0f0f0,#8c8b8b,#f0f0f0);'/>";
 
   public static final String GRN = "\u001B[32m";
+  public static final String RED = "\u001B[31m";
   public static final String NC = "\u001B[0m";
 }
